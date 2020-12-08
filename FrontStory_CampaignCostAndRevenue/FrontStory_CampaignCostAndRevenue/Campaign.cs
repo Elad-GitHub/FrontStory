085c75496ef55e2e4dd74242163835a1b06b44ab
Elad-GitHub <71940527+Elad-GitHub@users.noreply.github.com>
2020-12-08 12:38:54 +0200

    Add files via upload
    FrontStory

---
+++ FrontStory_CampaignCostAndRevenue/FrontStory_CampaignCostAndRevenue/Campaign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FrontStory_CampaignCostAndRevenue
+{
+    public class Campaign
+    {
+        public DateTime Date { get; set; }       
+        public string Name { get; set; }
+        public int Clicks { get; set; }
+        public double Cost { get; set; }
+        public double Revenue { get; set; }
+        public double UV { get; set; }
+        public double CPC { get; set; }
+        public double ROI { get; set; }
+        public double Profit { get; set; }
+    }
+}
